8ee8041bad20f2a09dc5c2b0a59930d755552afc
andrew-wilkes <github@urgentclick.com>
2023-10-04 08:26:17 +0900

    First pass implementation

--- StateMachine.gd
+++ StateMachine.gd
@@ -2,8 +2,6 @@ extends Node
 
 class_name StateMachine
 
-const DEBUG = true
-
 var current_state: Object
 
 var history = []
@@ -13,24 +11,24 @@ func _ready():
 	states = get_children()
 	# Set the initial state to the first child node
 	set_state(states[0].name)
-	# Allow for all nodes to be ready before calling _enter_state
-	#call_deferred("_enter_state")
 
 
 func change_to(new_state_id):
-	history.append(new_state_id)
-	call_deferred("set_state", new_state_id)
+	prints("change_to", new_state_id)
+	history.append(current_state.name)
+	#call_deferred("set_state", new_state_id)
+	set_state(new_state_id)
 
 
 
 func back():
 	if history.size() > 0:
-		call_deferred("set_state", history.pop_back())
+		var next_state = history.pop_back()
+		#call_deferred("set_state", next_state)
+		set_state(next_state)
 
 
 func _enter_state():
-	if DEBUG:
-		print("Entering state: ", current_state.name)
 	# Give the new state a reference to it's state machine i.e. this one
 	current_state.fsm = self
 	current_state.enter()
@@ -40,7 +38,7 @@ func set_state(state_name):
 	for a_state in states:
 		if a_state.name == state_name:
 			current_state = a_state
-			if not a_state.get_parent():
+			if a_state.get_parent() == null:
 				add_child(a_state)
 		else:
 			if a_state.get_parent():
